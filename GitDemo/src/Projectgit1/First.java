2035b5d5119cff02b2d37b50c69d88f215b2491e
vaibhavalone32 <vaibhavalone32@gmail.com>
2022-03-31 16:19:58 +0530

    code modified with methode name

--- GitDemo/src/Projectgit1/First.java
+++ GitDemo/src/Projectgit1/First.java
@@ -5,7 +5,9 @@ public class First {
 	public static void main(String[] args) {
 
 		System.out.println("welcome to java coding");
-		
-System.out.println("push to eclispe");
+
+		System.out.println("push to eclispe");
+
+		System.out.println("successfull send");
 	}
 }
